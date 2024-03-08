fd4b7ed8c8aef35dcd0c495f4e0758d8f51ac792
sviataslau <sviataslau.rudzik@bankifi.com>
2024-03-08 14:53:57 +0100

    Fix client codec

--- core/src/main/java/com/movie/expert/clients/ClientBuilder.java
+++ core/src/main/java/com/movie/expert/clients/ClientBuilder.java
@@ -1,13 +1,28 @@
 package com.movie.expert.clients;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.codec.json.Jackson2JsonDecoder;
+import org.springframework.web.reactive.function.client.ExchangeStrategies;
 import org.springframework.web.reactive.function.client.WebClient;
 
 @Configuration
 public class ClientBuilder {
+    @Autowired
+    private ObjectMapper baseMapper;
+
     @Bean
     public WebClient.Builder webClientBuilder() {
-        return WebClient.builder();
+        ObjectMapper newMapper = baseMapper.copy();
+        newMapper.setPropertyNamingStrategy(PropertyNamingStrategies.SNAKE_CASE);
+
+        ExchangeStrategies exchangeStrategies = ExchangeStrategies.builder()
+                .codecs(configurer ->
+                        configurer.defaultCodecs().jackson2JsonDecoder(new Jackson2JsonDecoder(newMapper)))
+                .build();
+        return WebClient.builder().exchangeStrategies(exchangeStrategies);
     }
 }
